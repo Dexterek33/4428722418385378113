b1cc7f2e486a8818f3d14e342fb89b2cf7f13ce5
Dexterek33 <anoface010@gmail.com>
2025-03-30 22:04:03 +0200

    service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker zainstalowany!');
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/cropped-192.png',
+        '/CompressedJPEG.Online_img(512x512).png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    console.log('Pobieranie:', event.request.url);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
