aefcfbc6d5a8b32afb3bba06f6d3af1611b4a12a
Dexterek33 <anoface010@gmail.com>
2025-04-02 21:56:49 +0200

    qrcode.js

--- js/qrcode.js
+++ js/qrcode.js
@@ -1,1 +1,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+
+
+// Funkcja generująca kod QR na podstawie tekstu
+function generateQRCode(text) {
+    qrDiv.innerHTML = ""; // Czyścimy poprzedni kod QR
+    new QRCode(qrDiv, {
+        text: text,  // Wstawiamy losowy tekst do wygenerowania kodu QR
+        width: 200,  // Ustalamy szerokość kodu QR
+        height: 200  // Ustalamy wysokość kodu QR
+    });
+}
